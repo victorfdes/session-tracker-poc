c611fd0ed30f65b1ba28935566b140d4291b54f3
Victor Fernandes <4434814+victorfdes@users.noreply.github.com>
2021-07-20 19:43:05 +0530

    update project config

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,25 @@
 import json from '@rollup/plugin-json'
 import typescript from '@rollup/plugin-typescript'
+import sourcemaps from 'rollup-plugin-sourcemaps'
 
 export default {
   input: 'src/main.ts',
-  output: {
-    name: 'main.js',
-    dir: 'dist',
-    format: 'umd'
-  },
+  output: [
+    {
+      name: 'main',
+      dir: 'dist',
+      format: 'esm'
+    },
+    {
+      name: 'browser',
+      file: 'dist/browser.js',
+      format: 'umd',
+      sourcemap: true
+    }
+  ],
   plugins: [
     json(),
-    typescript()
+    typescript(),
+    sourcemaps()
   ]
 }
