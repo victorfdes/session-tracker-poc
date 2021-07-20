2c1d2689404d5e5707822c688ab9f1284b90c2fe
Victor Fernandes <4434814+victorfdes@users.noreply.github.com>
2021-07-20 19:44:48 +0530

    add common config

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const SESSION_COOKIE_NAME: string = '_stp'
+export const SESSION_TIMEOUT: number = 30 // minutes
+export const GLOBAL_NAMESPACE = 'insticator'
+export enum SYSTEM_EVENTS {
+  PAGE_VIEW = 'page view'
+}
